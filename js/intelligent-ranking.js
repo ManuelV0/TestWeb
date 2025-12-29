032f36fa7a85f91d330a976ccfa17437d5c39cf4
ManuelV0 <manuel.voll69@icloud.com>
2025-12-29 16:46:34 +0100

    Update intelligent-ranking.js

--- js/intelligent-ranking.js
+++ js/intelligent-ranking.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 const SUPABASE_URL = "https://djikypgmchywybjxbwar.supabase.co";
-const SUPABASE_ANON_KEY = "TUO_ANON_KEY";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqaWt5cGdtY2h5d3lianhid2FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyMTMyOTIsImV4cCI6MjA2ODc4OTI5Mn0.dXqWkg47xTg2YtfLhBLrFd5AIB838KdsmR9qsMPkk8Q";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
